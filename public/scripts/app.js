0ff0b9c34ff2fec7afae2cfe886f18d74ccd7a40
Ghalib <gholibinf25@gmail.com>
2019-02-10 09:44:48 +0700

    Nesting Components - Method Binding

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,8 +8,48 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Header = function (_React$Component) {
-    _inherits(Header, _React$Component);
+var obj = {
+    name: 'Ghalib',
+    getName: function getName() {
+        return this.name;
+    }
+};
+
+var getName = obj.getName.bind(obj);
+
+console.log(getName());
+
+var GHLibApp = function (_React$Component) {
+    _inherits(GHLibApp, _React$Component);
+
+    function GHLibApp() {
+        _classCallCheck(this, GHLibApp);
+
+        return _possibleConstructorReturn(this, (GHLibApp.__proto__ || Object.getPrototypeOf(GHLibApp)).apply(this, arguments));
+    }
+
+    _createClass(GHLibApp, [{
+        key: 'render',
+        value: function render() {
+            var title = 'GHLib App';
+            var subtitle = 'Jadikan Coding sebagai Hobimu';
+            var options = ['Thing one', 'Thing two', 'Thing Three'];
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(Header, { title: title, subtitle: subtitle }),
+                React.createElement(Action, null),
+                React.createElement(Options, { options: options }),
+                React.createElement(AddOption, null)
+            );
+        }
+    }]);
+
+    return GHLibApp;
+}(React.Component);
+
+var Header = function (_React$Component2) {
+    _inherits(Header, _React$Component2);
 
     function Header() {
         _classCallCheck(this, Header);
@@ -20,10 +60,20 @@ var Header = function (_React$Component) {
     _createClass(Header, [{
         key: 'render',
         value: function render() {
+            console.log(this.props);
             return React.createElement(
-                'p',
+                'div',
                 null,
-                'This is React Paragraph'
+                React.createElement(
+                    'h1',
+                    null,
+                    this.props.title
+                ),
+                React.createElement(
+                    'h2',
+                    null,
+                    this.props.subtitle
+                )
             );
         }
     }]);
@@ -31,15 +81,150 @@ var Header = function (_React$Component) {
     return Header;
 }(React.Component);
 
-var jsx = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'Tittle'
-    ),
-    React.createElement(Header, null)
-);
+var Action = function (_React$Component3) {
+    _inherits(Action, _React$Component3);
 
-ReactDOM.render(jsx, document.getElementById('app'));
+    function Action() {
+        _classCallCheck(this, Action);
+
+        return _possibleConstructorReturn(this, (Action.__proto__ || Object.getPrototypeOf(Action)).apply(this, arguments));
+    }
+
+    _createClass(Action, [{
+        key: 'handlePick',
+        value: function handlePick() {
+            alert('alert');
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'button',
+                    { onClick: this.handlePick },
+                    'What Should I do?'
+                )
+            );
+        }
+    }]);
+
+    return Action;
+}(React.Component);
+
+var Options = function (_React$Component4) {
+    _inherits(Options, _React$Component4);
+
+    function Options(props) {
+        _classCallCheck(this, Options);
+
+        var _this4 = _possibleConstructorReturn(this, (Options.__proto__ || Object.getPrototypeOf(Options)).call(this, props));
+
+        _this4.handleRemoveAll = _this4.handleRemoveAll.bind(_this4);
+        return _this4;
+    }
+
+    _createClass(Options, [{
+        key: 'handleRemoveAll',
+        value: function handleRemoveAll() {
+            console.log(this.props.options);
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            //console.log(this.props.options.length);
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'button',
+                    { onClick: this.handleRemoveAll },
+                    'Remove All'
+                ),
+                React.createElement(
+                    'p',
+                    null,
+                    'Options Component Here'
+                ),
+                this.props.options.map(function (option) {
+                    return React.createElement(Option, { key: option, optionText: option });
+                })
+            );
+        }
+    }]);
+
+    return Options;
+}(React.Component);
+
+var Option = function (_React$Component5) {
+    _inherits(Option, _React$Component5);
+
+    function Option() {
+        _classCallCheck(this, Option);
+
+        return _possibleConstructorReturn(this, (Option.__proto__ || Object.getPrototypeOf(Option)).apply(this, arguments));
+    }
+
+    _createClass(Option, [{
+        key: 'render',
+        value: function render() {
+            console.log(this.props);
+            return React.createElement(
+                'div',
+                null,
+                this.props.optionText
+            );
+        }
+    }]);
+
+    return Option;
+}(React.Component);
+
+var AddOption = function (_React$Component6) {
+    _inherits(AddOption, _React$Component6);
+
+    function AddOption() {
+        _classCallCheck(this, AddOption);
+
+        return _possibleConstructorReturn(this, (AddOption.__proto__ || Object.getPrototypeOf(AddOption)).apply(this, arguments));
+    }
+
+    _createClass(AddOption, [{
+        key: 'handleAddOption',
+        value: function handleAddOption(e) {
+            e.preventDefault();
+
+            var option = e.target.elements.option.value;
+
+            if (option) {
+                alert(option);
+            }
+            e.target.elements.option.value = '';
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'form',
+                { onSubmit: this.handleAddOption },
+                React.createElement('input', { type: 'text', name: 'option' }),
+                React.createElement(
+                    'button',
+                    null,
+                    'Add Option'
+                )
+            );
+        }
+    }]);
+
+    return AddOption;
+}(React.Component);
+
+// const jsx = (
+//     <div>
+//         <GHLibApp />
+//     </div>
+// );
+
+ReactDOM.render(React.createElement(GHLibApp, null), document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -1,14 +1,119 @@
-class Header extends React.Component {
-    render() {
-        return <p>This is React Paragraph</p>;
+const obj = {
+    name: 'Ghalib',
+    getName(){
+        return this.name;
+    }
+};
+
+const getName = obj.getName.bind(obj);
+
+console.log(getName());
+
+class GHLibApp extends React.Component{
+    render(){
+        const title = 'GHLib App';
+        const subtitle = 'Jadikan Coding sebagai Hobimu';
+        const options = ['Thing one', 'Thing two', 'Thing Three'];
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle}/> 
+                <Action />
+                <Options options={options}/>
+                <AddOption />
+            </div>
+        );
     }
 }
 
-const jsx = (
-    <div>
-        <h1>Tittle</h1>
-        <Header />
-    </div>
-);
+class Header extends React.Component {
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
+            </div>
+        );
+    }
+}
 
-ReactDOM.render(jsx, document.getElementById('app'));
+class Action extends React.Component{
+    handlePick(){
+        alert('alert');
+    }
+
+    render(){
+        return (
+            <div>
+                <button onClick={this.handlePick}>What Should I do?</button>
+            </div>
+        );
+    }
+}
+
+class Options extends React.Component{
+     constructor(props){
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
+
+    handleRemoveAll(){
+        console.log(this.props.options);
+    }
+    
+    render(){
+        //console.log(this.props.options.length);
+        return (
+            <div>
+                <button onClick={this.handleRemoveAll}>Remove All</button>
+                <p>Options Component Here</p>
+                {
+                   this.props.options.map((option) => <Option key={option} optionText={option} />)
+                }
+            </div>
+        );
+    }
+}
+
+class Option extends React.Component {
+        
+    render(){
+        console.log(this.props);
+        return (
+            <div>
+                {this.props.optionText}               
+            </div> 
+        );
+    }
+}
+
+class AddOption extends React.Component{
+    handleAddOption(e){
+        e.preventDefault();
+        
+        const option = e.target.elements.option.value;
+
+        if(option){
+            alert(option);
+        }
+        e.target.elements.option.value = '';
+    }
+    
+    render(){
+        return (
+            <form onSubmit={this.handleAddOption}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        );
+    }
+}
+
+
+// const jsx = (
+//     <div>
+//         <GHLibApp />
+//     </div>
+// );
+
+ReactDOM.render(<GHLibApp />, document.getElementById('app'));
